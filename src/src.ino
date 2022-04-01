43d97aa0ed790f34b38fdfabbaee892ed6b56659
Michał Pawłowski <michpaw199@student.polsl.pl>
2022-04-01 16:21:28 +0000

    Merged PR 5: feature/wifi-connection
    feature: connect to wifi

--- src/src.ino
+++ src/src.ino
@@ -1,12 +1,14 @@
+#include "network.h"
+
 int led = 32;
 
 void setup() {
-  
+  Serial.begin(115200);
   pinMode(led, OUTPUT);
+  initNetwork();
 }
 
 void loop() {
-  
   digitalWrite(led, HIGH);
   delay(1000);
   digitalWrite(led, LOW);
